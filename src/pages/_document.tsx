ab4718ad3f9d6ce3a7ff73d5e87c455139cdbfdd
Daryll Ko <daryll.ko@gmail.com>
2023-03-25 21:32:37 +0800

    fix: use Next `_document` for katex stylesheet

---
+++ src/pages/_document.tsx
@@ -0,0 +1,22 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta name="description" content="Web-based SN P system simulator" />
+        <link rel="icon" href="/favicon.svg" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.css"
+          integrity="sha384-Xi8rHCmBmhbuyyhbI88391ZKP2dmfnOl4rT9ZfRI7mLTdk1wblIUnrIq35nqwEvC"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
