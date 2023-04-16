94a6023c9b298966f5e7d7fd6f5fd19f07302abb
Daryll Ko <daryll.ko@gmail.com>
2023-04-16 14:52:14 +0800

    feat: add `defaultSelected`

--- src/types/Selected.ts
+++ src/types/Selected.ts
@@ -4,4 +4,11 @@ interface Selected {
   synapse: number;
 }
 
+const defaultSelected: Selected = {
+  neuron: 1,
+  rule: 0,
+  synapse: 0,
+};
+
 export default Selected;
+export { defaultSelected };
