31dc738a25b281222b9cc0aa88c59dff02c8b3e7
Daryll Ko <daryll.ko@gmail.com>
2023-04-20 09:40:56 +0800

    feat: make consumed part also display lambda if 0

--- src/utils/getRuleString.ts
+++ src/utils/getRuleString.ts
@@ -3,11 +3,12 @@ import type Rule from "~/types/Rule";
 const getRuleString = (rule: Rule) => {
   const { regex, consumed, produced, delay } = rule;
 
-  const consumption = `${consumed !== 1 ? `a^{${consumed}}` : "a"}`;
-  const production = `${
-    produced > 1 ? `a^{${produced}}` : `${produced === 1 ? "a" : "\\lambda"}`
-  }`;
-  const ruleString = `${regex}/${consumption} \\rightarrow ${production}; ${delay}`;
+  const getToken = (amount: number): string =>
+    amount > 1 ? `a^{${amount}}` : `${amount === 1 ? "a" : "\\lambda"}`;
+
+  const ruleString = `${regex}/${getToken(consumed)} \\rightarrow ${getToken(
+    produced
+  )}; ${delay}`;
 
   return ruleString;
 };
